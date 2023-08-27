b94349b17a41c34466220be9698156baeabf8e5d
Felipe Escalona <escalonaf12@gmail.com>
2023-08-27 08:30:29 -0400

    feat( Custom logger): adding custom logger ✨

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import app from "./app.js";
-
-import dotenv from "dotenv";
+import logger from "./configs/logger.js";
 
 //env variables
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}...`);
+  logger.info(`Server running on port ${PORT}...`);
 });
